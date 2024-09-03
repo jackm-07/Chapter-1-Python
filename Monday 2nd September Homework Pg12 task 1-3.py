e9341ada7d41cf9aa1fbb3222327081c46024a90
jackm-07 <ctw20.jack.maher@corketb.ie>
2024-09-03 19:34:36 +0100

    Add files via upload
    The homework I completed on the 2nd of september 2024 and it involved using the print function to do various tasks and make shapes

---
+++ Monday 2nd September Homework Pg12 task 1-3.py
@@ -0,0 +1,50 @@
+# Jack Maher
+# Monday, 02-09-24 Homework
+# Page 12 Tasks 1-3
+
+
+# task 1
+print("Goodbye", end="")
+print("\t", "Goodbye")
+
+# task 2
+print("No way", end= ".")
+
+# task 3: face
+# plan is to use O for eyes and L for a nose and then \___/ for a smile
+
+print("\n\n", "O", "\t", "O")
+print("     L   ")
+print("   \___/")
+
+# task 3: letter x
+# plan is to go \/
+#               /\
+# it didn't allow me to type "/\ so the new plan is just to use >< 
+
+print("\n\n\t><")
+
+#task 3: triangle
+#plan is to  make /\
+#                /__\
+
+print("\n\t /\\")
+# i looked it up and turns out all I needed to do was add another backslash
+print("\t/__\\")
+
+#task 3: small circle
+#plan is to keep it simple with ()
+
+print("\n\n\t()")
+
+#task 3: large circle
+# plan is to do
+# _---_
+#[     ]
+#|     |
+# -___-
+
+print(" _---_")
+print("[     ]")
+print("[     ]")
+print(" -___-")
