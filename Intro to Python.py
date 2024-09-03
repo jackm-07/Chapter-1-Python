cee2046ca0343d3692760f4434729f09a59f62f8
jackm-07 <ctw20.jack.maher@corketb.ie>
2024-09-03 13:45:49 +0100

    Add files via upload
    This was the first thing we did in CS on the 2nd of September and we went over the basics of the print function

---
+++ Intro to Python.py
@@ -0,0 +1,33 @@
+#Author: Jack Maher
+#Date: 02-09-24
+#Intro to Python: Print statement
+print("Hello World!")
+# You put the command 'print' then ( and quotation then your desired phrase end quotation )
+print("My name is")
+# if quotations aren't used then the computer assumes the word or phrase is a variable
+
+
+name = "Jack"
+surname = "Maher"
+print(name, surname, "\n")
+
+print("What is your age")
+print(21)
+print("My age is 21")
+print("My age is", "21")
+print("I am", "18", "and my friend is", "21")
+print("The print command", "can handle", "multiple strings\n")
+
+
+print("A\tb\nC\td\n")
+# t must mean tab and n must mean enter
+
+print("As I was going out one day")
+print("My head fell off and rolled away")
+print("But when I saw it was gone")
+print("I picked it up and put it on\n")
+
+counter = 10
+while counter < 501:
+    print(counter)
+    counter = counter*2
