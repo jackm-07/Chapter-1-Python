90defa33b03d2da53ecfade0ca32674caf41e519
jackm-07 <ctw20.jack.maher@corketb.ie>
2024-09-06 13:26:03 +0100

    Add files via upload
    Homework that was completed on the 4th of September

---
+++ Wednesday 4th September Homework Pg12 Q11-26 & Task 4.py
@@ -0,0 +1,32 @@
+# Author: Jack Maher
+# Date: 4th September 2024
+
+# Task 4
+# the plan is
+#             \/ \/ \/
+#             /\ /\ /\
+#                \/   
+#                /\   
+#             \/ \/   
+#             /\ /\
+print(" \/ \/ \/", "\n", "/\\ /\\ /\\") #line 1 of x's
+print("    \/", "\n", "   /\\",) #second line of x
+print(" \/ \/", "\n", "/\\ /\\")
+
+#Q's 11-26
+#11: print("I've had enough.")
+#12: it refers to your punctuation
+#13: if you don't then the program will not run
+#14: a bug is a logical error
+#15: a string is what is within "" marks
+#16: it means that the computer does not try run what's in the string as code
+#17: adding additional instructions inside the brackets
+#18: parameter
+#19: a space?
+#20: \n enter \t tab
+#21 \\
+#22 \n
+#23: add it twice
+#24: to help other coders understand the code
+#25: single line and multiline (in hindsight could have used multiline here)
+#26: # for single line """ at top and bottom for multiline
