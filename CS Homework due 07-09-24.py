fb3f673d84c33b2a6fe61a8823d92cbf9a1e9624
jackm-07 <ctw20.jack.maher@corketb.ie>
2024-10-09 11:17:18 +0100

    Add files via upload
    classwork of 7/8 october

---
+++ CS Homework due 07-09-24.py
@@ -0,0 +1,108 @@
+#Author: Jack Maher
+#Date: 06-10-24
+#Homework Tasks1-2
+
+# n1=int(input("Enter your first number:"))
+# n2=int(input("Enter your second number:"))
+# nDivided=n1/n2
+# typeOfValue=type(nDivided)
+# if typeOfValue=='int':
+#     print("!")
+# else:
+#     print("?")
+    
+# driverAge=int(input("Enter your age in years (number form) :"))
+# if driverAge>16:
+#     print("You are eligible for a driver's license")
+# else:
+#     print("You are too young to be eligible for a driver's license")
+
+#Task3
+# itemCost=int(input("Enter the cost of the item (number only):"))
+# if itemCost>10000: #if its less more than 10000
+#     print("Go to tender")
+# elif itemCost>499: #if more than 500 less than 10000
+#     print("get quotes from 3 other suppliers")
+# else: #else if under 500
+#     print("Item Ordered")
+
+#Task 4
+# letterSelected=int(input("Enter:\n(1) for A\n(2) for B\n(3) for C\n:"))
+# if letterSelected==1: #option a
+#     print("You have won a free ticket to Dundrum Shopping Cenre!")
+# elif letterSelected==2:option b
+#     print("You have won a free trip to Tallaght!")
+# else: #option c
+#     print("You have won a free trip to Broombridge!")
+
+#Task 5
+# examResult=int(input("Please enter your exam result:"))
+# if examResult>=101: #checking for scammers
+#     print("liar")
+# elif examResult>=90: 90-100=h1
+#     print("H1")
+# elif examResult>=80: 80-89=h2
+#     print("H2")
+# elif examResult>=70: 70-79=h3
+#     print("H3")
+# elif examResult>=60: 60-69=h4
+#     print("H4")
+# elif examResult>=50: 50-59=h5
+#     print("H5")
+# elif examResult>=40: 40-49=h6
+#     print("H6")
+# elif examResult>=30: 30-39=h7
+#     print("H7")
+# elif examResult>=0: 0-29= h8
+#     print("H8")
+
+
+#taks 1
+# name=len(input("Write your name:"))
+# if name>10: #above 10
+#     print("new name please too long")
+# elif name>7: #above 7
+#     print("get a new shorter nickname")
+# else: #
+#     print("your name is very convenient storage-wise")
+    
+#task2
+# decision=int(input("Menu\n1. Music\n2. History\n3. Design and Technology\n4. Exit\nPlease enter your decision:"))
+# if decision==1:
+#     print("You chose Music")
+# elif decision==2:
+#     print("You chose History")
+# elif decision==3:
+#     print("You chose Design and Technology")
+# else:
+#     print("Goodbye")
+
+#task 3
+# import random
+# n1=random.randint(1,6)
+# n2=random.randint(1,6)
+# if n1==n2:
+#     runningTotal=2*(n1+n2)
+#     print("You got a double, score is: "+str(runningTotal))
+# else:
+#     runningTotal=n1+n2
+#     print("Score is: "+str(runningTotal))
+
+#task 4
+# price=float(input("Enter how much money you spent in me shop in numbers:"))
+# if price>=200:
+#     price=round(price*0.9,2)
+# elif price>=100:
+#     price=round(price*0.95,2)
+# print(price)
+
+#task 5
+# n1=int(input("Enter a number:"))
+# n2=int(input("Enter a number:"))
+# if n1>n2:
+#     difference=n1-n2
+# else:
+#     difference=n2-n1
+# print("The difference is: "+str(difference))
+#
+
